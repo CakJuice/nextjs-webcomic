ad65ca03828b3cd23ba4c85d7784d1d135685cfd
Hendra juice <hd.brandoz@gmail.com>
2019-01-10 20:53:42 +0700

    install axios

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,26 @@
-const Index = () => (
+import axios from 'axios'
+
+const Index = (props) => (
+  <div>
     <h1 className="title">Homepage</h1>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {
+        props.shows.map(({show}) => (
+          <li key={show.id}>
+            {show.name}
+          </li>
+        ))
+      }
+    </ul>
+  </div>
 )
+
+Index.getInitialProps = async function() {
+  const response = await axios.get('https://api.tvmaze.com/search/shows?q=batman')
+  return {
+    shows: await response.data
+  }
+}
 
 export default Index
