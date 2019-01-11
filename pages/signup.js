9ebc1f44f780f9d781bda776ac144b7ba5e4075a
Hendra Juice <hd.brandoz@gmail.com>
2019-01-11 13:01:01 +0700

    test post signup form

--- pages/signup.js
+++ pages/signup.js
@@ -1,14 +1,58 @@
 import { Component } from 'react'
+import axios from 'axios'
 
 class Signup extends Component {
   constructor(props) {
     super(props)
-    this.state = {
+    this.state = this._getInitialState()
+
+    this._onchangeUsername = this._onchangeUsername.bind(this)
+    this._onchangeEmail = this._onchangeEmail.bind(this)
+    this._onchangePassword1 = this._onchangePassword1.bind(this)
+    this._onchangePassword2 = this._onchangePassword2.bind(this)
+    this._submitForm = this._submitForm.bind(this)
+  }
+
+  _getInitialState() {
+    return {
       username: '',
       email: '',
       password1: '',
       password2: '',
     }
+  }
+
+  _onchangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  _onchangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  _onchangePassword1(e) {
+    this.setState({
+      password1: e.target.value
+    })
+  }
+
+  _onchangePassword2(e) {
+    this.setState({
+      password2: e.target.value
+    })
+  }
+
+  _submitForm(e) {
+    e.preventDefault()
+    axios.post('/signup', this.state).then((response) => {
+      console.log(response)
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 
   render() {
@@ -17,41 +61,45 @@ class Signup extends Component {
         <h1 className="title">Signup New User</h1>
         <div className="columns">
           <div className="column is-one-third">
-            <form method="POST" noValidate>
+            <form method="POST" onSubmit={this._submitForm} noValidate>
               <div className="field">
                 <label className="label">Username</label>
                 <div className="control">
-                  <input className="input" type="text" placeholder="Your username"/>
+                  <input className="input" type="text" onChange={this._onchangeUsername}
+                    value={this.state.username} placeholder="Your username"/>
                 </div>
               </div>
               <div className="field">
                 <label className="label">Email</label>
                 <div className="control">
-                  <input className="input" type="email" placeholder="Your email"/>
+                  <input className="input" type="email" onChange={this._onchangeEmail}
+                    value={this.state.email} placeholder="Your email"/>
                 </div>
               </div>
               <div className="field">
                 <label className="label">Password</label>
                 <div className="control">
-                  <input className="input" type="password" placeholder="Your password"/>
+                  <input className="input" type="password" onChange={this._onchangePassword1}
+                    value={this.state.password1} placeholder="Your password"/>
                 </div>
               </div>
               <div className="field">
                 <label className="label">Password Confirmation</label>
                 <div className="control">
-                  <input className="input" type="password" placeholder="Confirm your password"/>
+                  <input className="input" type="password" onChange={this._onchangePassword2}
+                    value={this.state.password2} placeholder="Confirm your password"/>
                 </div>
               </div>
-              <div class="field">
-                <div class="control">
-                  <label class="checkbox">
+              <div className="field">
+                <div className="control">
+                  <label className="checkbox">
                     <input type="checkbox"/> I agree to the <a href="#">terms and conditions</a>
                   </label>
                 </div>
               </div>
-              <div class="field">
-                <div class="control">
-                  <button class="button is-link">Submit</button>
+              <div className="field">
+                <div className="control">
+                  <button className="button is-link">Submit</button>
                 </div>
               </div>
             </form>
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const next = require('next')
+const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+// const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json()
 
 app.prepare().then(() => {
   const server = express()
+
+  server.post('/signup', jsonParser, (req, res) => {
+    console.log(req.body)
+    return res.json({status: true})
+  })
+
   server.get('*', (req, res) => {
     return handle(req, res)
   })
