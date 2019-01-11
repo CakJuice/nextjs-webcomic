4dd892f1fee6aa59ac29408b90a41f0c5b0b34fc
Hendra Juice <hd.brandoz@gmail.com>
2019-01-11 15:27:34 +0700

    add validation signup

--- pages/signup.js
+++ pages/signup.js
@@ -19,6 +19,34 @@ class Signup extends Component {
       email: '',
       password1: '',
       password2: '',
+      errorUsername: [],
+      errorEmail: [],
+      errorPassword1: [],
+      errorPassword2: []
+    }
+  }
+
+  _resetValueState() {
+    this.setState({
+      username: '',
+      email: '',
+      password1: '',
+      password2: ''
+    })
+  }
+
+  _resetErrorState() {
+    this.state.errorUsername = []
+    this.state.errorEmail = []
+    this.state.errorPassword1 = []
+    this.state.errorPassword2 = []
+  }
+
+  _getPostData() {
+    return {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password1
     }
   }
 
@@ -26,33 +54,91 @@ class Signup extends Component {
     this.setState({
       username: e.target.value
     })
+
+    if (this.state.errorUsername.length > 0) this.state.errorUsername = []
   }
 
   _onchangeEmail(e) {
     this.setState({
       email: e.target.value
     })
+
+    if (this.state.errorEmail.length > 0) this.state.errorEmail = []
   }
 
   _onchangePassword1(e) {
     this.setState({
       password1: e.target.value
     })
+
+    if (this.state.errorPassword1.length > 0) this.state.errorPassword1 = []
   }
 
   _onchangePassword2(e) {
     this.setState({
       password2: e.target.value
     })
+
+    if (this.state.errorPassword2.length > 0) this.state.errorPassword2 = []
+  }
+
+  _validate() {
+    let isValid = true
+
+    if (this.state.username == '') {
+      let error = [...this.state.errorUsername]
+      error.push("Username required")
+      this.setState({
+        errorUsername: error
+      })
+      isValid = false
+    }
+
+    if (this.state.email == '') {
+      let error = [...this.state.errorEmail]
+      error.push("Email required")
+      this.setState({
+        errorEmail: error
+      })
+      isValid = false
+    }
+
+    if (this.state.password1 == '') {
+      let error = [...this.state.errorPassword1]
+      error.push("Password required")
+      this.setState({
+        errorPassword1: error
+      })
+      isValid = false
+    }
+
+    if (this.state.password2 == '') {
+      let error = [...this.state.errorPassword2]
+      error.push("Password confirmation required")
+      this.setState({
+        errorPassword2: error
+      })
+      isValid = false
+    }
+
+    return isValid
   }
 
   _submitForm(e) {
     e.preventDefault()
-    axios.post('/signup', this.state).then((response) => {
-      console.log(response)
-    }).catch((error) => {
-      console.log(error)
-    })
+
+    this._resetErrorState()
+
+    if (this._validate()) {
+      const data = this._getPostData()
+      axios.post('/signup', data).then((response) => {
+        console.log(response)
+      }).catch((error) => {
+        console.log(error)
+      })
+    } else {
+      console.log(this.state)
+    }
   }
 
   render() {
@@ -65,30 +151,46 @@ class Signup extends Component {
               <div className="field">
                 <label className="label">Username</label>
                 <div className="control">
-                  <input className="input" type="text" onChange={this._onchangeUsername}
-                    value={this.state.username} placeholder="Your username"/>
+                  <input className={"input" + (this.state.errorUsername.length > 0 ? ' is-danger' : '')}
+                    type="text" onChange={this._onchangeUsername} value={this.state.username}
+                    placeholder="Your username"/>
                 </div>
+                {
+                  this.state.errorUsername.map((errorMsg, idx) => <p key={idx} className="help is-danger">{errorMsg}</p>)
+                }
               </div>
               <div className="field">
                 <label className="label">Email</label>
                 <div className="control">
-                  <input className="input" type="email" onChange={this._onchangeEmail}
-                    value={this.state.email} placeholder="Your email"/>
+                  <input className={"input" + (this.state.errorEmail.length > 0 ? ' is-danger' : '')}
+                    type="email" onChange={this._onchangeEmail} value={this.state.email}
+                    placeholder="Your email"/>
                 </div>
+                {
+                  this.state.errorEmail.map((errorMsg, idx) => <p key={idx} className="help is-danger">{errorMsg}</p>)
+                }
               </div>
               <div className="field">
                 <label className="label">Password</label>
                 <div className="control">
-                  <input className="input" type="password" onChange={this._onchangePassword1}
-                    value={this.state.password1} placeholder="Your password"/>
+                  <input className={"input" + (this.state.errorPassword1.length > 0 ? ' is-danger' : '')}
+                    type="password" onChange={this._onchangePassword1} value={this.state.password1}
+                    placeholder="Your password"/>
                 </div>
+                {
+                  this.state.errorPassword1.map((errorMsg, idx) => <p key={idx} className="help is-danger">{errorMsg}</p>)
+                }
               </div>
               <div className="field">
                 <label className="label">Password Confirmation</label>
                 <div className="control">
-                  <input className="input" type="password" onChange={this._onchangePassword2}
-                    value={this.state.password2} placeholder="Confirm your password"/>
+                  <input className={"input" + (this.state.errorPassword2.length > 0 ? ' is-danger' : '')}
+                    type="password" onChange={this._onchangePassword2} value={this.state.password2}
+                    placeholder="Confirm your password"/>
                 </div>
+                {
+                  this.state.errorPassword2.map((errorMsg, idx) => <p key={idx} className="help is-danger">{errorMsg}</p>)
+                }
               </div>
               <div className="field">
                 <div className="control">
